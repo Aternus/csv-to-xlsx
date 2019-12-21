75a9a8263722200a3cb49212debe4721ad273f2e
Kiril Reznik <kiril@atern.us>
2019-12-21 22:19:37 +0200

    1.0.12 - CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ### Security
 
+## [1.0.12]
+
+### Security
+
+- Updated npm dependencies
+
 ## [1.0.11]
 
 ### Changed
--- publish.sh
+++ publish.sh
@@ -10,29 +10,29 @@ VERSION=$1
 # Helper Functions
 ##
 print_info() {
-    # blue
-    echo -e "\e[96m-=> $1\e[39m"
+  # blue
+  echo -e "\e[96m-=> $1\e[39m"
 }
 
 print_success() {
-    echo -e "\e[92m-=> $1\e[39m"
+  echo -e "\e[92m-=> $1\e[39m"
 }
 
 ##
 # Functions
 ##
-version () {
-    print_info "Updating version..."
-    # must be run on a clean git directory
-    # see package.json for preversion, version and postversion scripts
-    # git: auto commit
-    # git: auto tag
-    npm version --force "${VERSION}" -m "%s - CHANGELOG.md"
+version() {
+  print_info "Updating version..."
+  # must be run on a clean git directory
+  # see package.json for preversion, version and postversion scripts
+  # git: auto commit
+  # git: auto tag
+  npm version --force "${VERSION}" -m "%s - CHANGELOG.md"
 }
 
 publish() {
-    print_info "Publishing to npmjs.com..."
-    npm publish
+  print_info "Publishing to npmjs.com..."
+  npm publish
 }
 
 ##
