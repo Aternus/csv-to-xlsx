437a6808dfa2bc8b42fc72cb17024e3636f72834
Aternus <kiril@atern.us>
2025-08-15 23:31:57 +0300

    feat(node): node v22 - updated jest docs

--- jest.config.js
+++ jest.config.js
@@ -5,16 +5,16 @@
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  // Automatically clear mock calls, instances, contexts and results before every test
+  // Automatically clear mock calls, instances, contexts and results before every test.
   clearMocks: true,
 
-  // Indicates which provider should be used to instrument code for coverage
+  // Indicates which provider should be used to instrument code for coverage. Allowed values are babel (default) or v8.
   coverageProvider: 'v8',
 
-  // The test environment that will be used for testing
+  // The test environment that will be used for testing.
   testEnvironment: 'node',
 
-  // A map from regular expressions to paths to transformers
+  // A map from regular expressions to paths to transformers.
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
